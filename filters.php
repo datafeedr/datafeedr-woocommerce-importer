0ea6766fe67f8774fcc655e568427fba6ff76155
Eric <ericbusch@gmail.com>
2020-10-20 11:40:51 -0400

    fixed a few bugs

--- filters.php
+++ filters.php
@@ -7,7 +7,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// @todo rename function
+/**
+ * This filter takes the current WooCommerce Product, finds other Product Sets that
+ * it might be associated with and adds those Product Sets' category IDs to this
+ * product's array of $category_ids.
+ *
+ * @param array $category_ids
+ * @param Dfrpswc_Product_Update_Handler $updater
+ *
+ * @return array
+ */
 function dfrpswc_append_category_ids_from_other_product_sets( array $category_ids, Dfrpswc_Product_Update_Handler $updater ) {
 
 	$product_set_ids = get_post_meta( $updater->wc_product->get_id(), '_dfrps_product_set_id', false );
